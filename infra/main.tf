627369d8f08973bb9ee9ff022ea7d69fd383956d
Sunaga0709 <tsunaga0709@gmail.com>
2025-06-01 04:01:20 +0900

    delete dynamodb_table

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "sunaga-sample"
-    key            = "go-ecs/terraform.tfstate"
-    region         = "ap-northeast-1"
-    dynamodb_table = "sunaga-sample-go-ecs-tf-lock"
-    encrypt        = true
+    bucket       = "sunaga-sample"
+    key          = "go-ecs/terraform.tfstate"
+    region       = "ap-northeast-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
